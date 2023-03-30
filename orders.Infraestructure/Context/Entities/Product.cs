329a595a68e38507b5ddef7d6f59e86df885f430
Omar Guanoluisa <omar.guanoluisa25@gmail.com>
2023-03-29 23:17:02 -0500

    the entities were created

---
+++ orders.Infraestructure/Context/Entities/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace orders.Infrastructure.Context.Entities
+{
+	[Table("Product")]
+	public class Product : BaseEntity
+	{
+		[Column("Name")]
+		public string Name { get; set; } = string.Empty;
+		[Column("Description")]
+		public string Description { get; set; } = string.Empty;
+		[Column("Price")]
+		public decimal Price { get; set; }
+
+		public ICollection<Order> Orders { get; set; }
+		public List<ProductsOrder> productsOrders { get; set; }
+
+	}
+}
+
